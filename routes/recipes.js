0101825df1506f48c201f562b39bfef70f277e8b
maya diretti <maya.diretti@icloud.com>
2024-02-20 16:18:25 +0100

    feat / cartella models

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,20 @@
 import express  from "express";
+import Recipe from "../models/recipe";
 
 const router = express.Router();
 
-router.use()
-router.get()
-router.post()
+router.post('/', async (req, res) => {
+
+    if(!req.body){
+        return res.status(400).send('you must send a recipe! ');
+    }
+    
+    try{
+        const recipe = await recipe.create(req.body);
+        return res.send(Recipe)
+    }catch (error){
+        return res.status(400).send(error.message);
+    }
+});
 
 export default router;
